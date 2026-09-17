5291a77d0471b3affd0b85ec3664f45db6bb297d
AndyKhan007 <AndyKhan007@users.noreply.github.com>
2026-09-17 10:29:35 +0800

    add web app script url

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Ganti dengan milik Anda setelah setup.
 export const CONFIG = {
   // URL Web App Apps Script (di-deploy "Anyone")
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/XXXXX/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby6OdqTqNygoDH3NIqqS_P52IedHx22FgYn8K5VuSTKwn0i9xRchjUnC5_0jDc9CWek/exec',
 
   // Google OAuth Client ID (dari Google Cloud Console)
   GOOGLE_CLIENT_ID: '871874183850-hgun5ou2n488lbnb1b5e4ob65jnn28j0.apps.googleusercontent.com',
